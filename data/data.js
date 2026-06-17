ee6ecd21a2f35c60c64d65a71a34f0f9cbab3ac3
Ludwig <ludwig.wagner@envidual.com>
2026-06-17 11:56:22 +0200

    Anpassungen

--- data/data.js
+++ data/data.js
@@ -97,7 +97,7 @@ const PADEL_DATA = {
     { id: "spiel5",  spieltag: 2, datum: "2026-06-10", uhrzeit: "12.00", team1: { spieler: ["Lukas P.",    "Martin B."]   }, team2: { spieler: ["Luca W.",      "Andreas L."]}, ergebnis: "0:6, 4:6", saetze: "0:2", sieger: 2 },
     { id: "spiel6",  spieltag: 2, datum: "2026-05-19", uhrzeit: "13.00", team1: { spieler: ["Ludwig W.",   "Cristian B."] }, team2: { spieler: ["Niklas K.",    "Greta P."]  }, ergebnis: "7:5, 6:1", saetze: "2:0", sieger: 1 },
     { id: "spiel7",  spieltag: 2, datum: "2026-06-12", uhrzeit: "07.30", team1: { spieler: ["Christoph L.","Raphael H."]  }, team2: { spieler: ["Marco M.",     "Marcel M."] }, ergebnis: "1:6, 1:6", saetze: "0:2", sieger: 2 },
-    { id: "spiel8",  spieltag: 2, datum: "2026-06-22", uhrzeit: "12.30", team1: { spieler: ["Chris M.",    "Jonas L."]    }, team2: { spieler: ["Irene W.",     "Leonie R."] }, ergebnis: null, saetze: null, sieger: null },
+    { id: "spiel8",  spieltag: 2, datum: "2026-06-17", uhrzeit: "12.00", team1: { spieler: ["Chris M.",    "Jonas L."]    }, team2: { spieler: ["Irene W.",     "Leonie R."] }, ergebnis: null, saetze: null, sieger: null },
     // SPIELTAG 3
     { id: "spiel9",  spieltag: 3, datum: "2026-06-08", uhrzeit: null, team1: { spieler: ["Cristian B.", "Chris M."]    }, team2: { spieler: ["Raphael H.",   "Leonie R."] }, ergebnis: null, saetze: null, sieger: null },
     { id: "spiel10", spieltag: 3, datum: "2026-06-08", uhrzeit: null, team1: { spieler: ["Agnes K.",    "Lukas P."]    }, team2: { spieler: ["Ludwig W.",    "Marco M."]  }, ergebnis: null, saetze: null, sieger: null },
